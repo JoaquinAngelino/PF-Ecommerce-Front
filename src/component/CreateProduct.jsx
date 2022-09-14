4cadcf5308ea79bbb52246dfd3909b91dabd618e
Fede Valdez <83725367+fvaldezz96@users.noreply.github.com>
2022-09-14 16:48:39 +0200

    CreateProduct�

--- src/component/CreateProduct.jsx
+++ src/component/CreateProduct.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const CreateProduct = () => {
-  return (
-    <div>CreateProduct</div>
-  )
+   return (
+      <div>
+         <h1>CreateProduct</h1>
+         <form action="">
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <div>
+               <button>submit</button>
+            </div>
+         </form>
+      </div>
+   )
 }
 
 export default CreateProduct
