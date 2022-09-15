ff55e991598dccee9d507eef9d6ee182e878228e
Fede Valdez <83725367+fvaldezz96@users.noreply.github.com>
2022-09-16 00:37:00 +0200

    addLogica

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCelulares } from '../../redux/actions';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../CardContainer/CardContainer.css';
 
 const CardContainer = () => {
+  const dispatch = useDispatch();
+  const allCards = useSelector((state) => state.allCell);
+
+  useEffect(() => {
+    dispatch(getCelulares())
+  })
+
   return (
-      <div className="card">
-        <img className="card-img-top col-6" src="#" alt="Card image cap"></img>
-        <div className="card-body-columns m-4">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-        </div>
+    <div>
+      <div >
+        {allCards?.map((e, index) => {
+          return (
+            <div key={index} className='card'>
+              <div>
+                <img className="card-img-top col-6" src="#" alt="Card image cap">{e.image}</img>
+              </div>
+              <div className="card-body-columns m-4">
+                <h3 className="card-title">{e.line}</h3>
+                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          )
+        })}
       </div>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import CardContainer from "../../components/CardContainer/CardContainer";
 import Pagination from "../../components/Pagination/Pagination";
 
-
 export default function Home() {
 
   return (
