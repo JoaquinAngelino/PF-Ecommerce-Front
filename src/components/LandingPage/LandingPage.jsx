65b4fc133606ce4790b43a41584e57ff9957ba90
juandavid84 <juandavid614@hotmail.com>
2022-09-15 15:59:30 -0500

    ultimos cambios

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,18 +1,13 @@
-import React from "react";
-import { Carousel } from 'react-responsive-carousel';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { Link } from "react-router-dom";
+import React from 'react'
 
 export default function LandingPage(){
-    const dispatch = useDispatch()
+
     return(
         <div>
-        <h1 id="h1l">Welcome to Cells Store</h1>
-        <Link  to= '/home'>
-        <button type="button" className="atras">Go Back</button>
-        </Link>
-        </div>
+      <h1>Hi</h1>
+      <div>
+      <button>Back</button>
+      </div>
+      </div>
     )
-
-}
+  }
