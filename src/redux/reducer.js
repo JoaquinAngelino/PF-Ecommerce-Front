30124a5e4112cd369d760b51c948f614f2ec878c
JoaquinAngelino <joacoangelino@gmail.com>
2022-09-15 21:19:24 -0300

    changes

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,19 +1,30 @@
 const {
-  GET_ALL_PRODUCTS
+  GET_ALL_PRODUCTS,
+  ACTIVE_LOADING,
 } = require('./actions.js')
 
 const initialState = {
-  products: []
+  products: [],
+  isLoading: true
 }
 
-export default function rootReducer(state = initialState, {type, payload}) {
+export default function rootReducer(state = initialState, { type, payload }) {
   switch (type) {
     case GET_ALL_PRODUCTS:
+      if (!payload) { return state }
       return {
         ...state,
-        products: payload
+        products: payload,
+        isLoading: false
+      }
+    //-----------------
+    //-----------------
+    case ACTIVE_LOADING:
+      return {
+        ...state,
+        isLoading: true
       }
     default:
-       return state
+      return state
   }
 }
